c6e4c3cc6854f3e62892be8b33d40317f15c0097
Philip He <philiphe5704@gmail.com>
2025-10-14 20:59:28 -0400

    Minor code change

--- lib/ProfilePage.dart
+++ lib/ProfilePage.dart
@@ -18,38 +18,39 @@ class ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("Profile Page")),
+
         body: Center(child:
-        Column(mainAxisAlignment: MainAxisAlignment.center,
+          Column(mainAxisAlignment: MainAxisAlignment.center,
             children: [
 
               Text("Welcome back, ${DataRepository.login}"),
 
-              Padding(child:
+              //Padding(child:
               TextField(
                 //controller: login,
                   decoration:
                   InputDecoration(
                       border: OutlineInputBorder(),
                       hintText: 'First Name')),
-                  padding: EdgeInsets.fromLTRB(50, 0, 50, 0)),
+                  //padding: EdgeInsets.fromLTRB(50, 0, 50, 0)),
 
               SizedBox(height: 20),
 
-              Padding(child:
+              //Padding(child:
               TextField(
                 //controller: login,
                   decoration:
                   InputDecoration(
                       border: OutlineInputBorder(),
                       hintText: 'Last Name')),
-                  padding: EdgeInsets.fromLTRB(50, 0, 50, 0)),
+                  //padding: EdgeInsets.fromLTRB(50, 0, 50, 0)),
 
               SizedBox(height: 20),
 
               Row(children: [
-                Padding(
-                padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
-                child:
+                //Padding(
+                //padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
+                //child:
                 Flexible(child:
                 TextField(
                   //controller: login,
@@ -57,7 +58,7 @@ class ProfilePageState extends State<ProfilePage> {
                     InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: 'Phone Number')),
-                    )),
+                    ),
 
                 ElevatedButton(
                     onPressed: () {},
@@ -74,9 +75,9 @@ class ProfilePageState extends State<ProfilePage> {
               SizedBox(height: 20),
 
               Row(children: [
-                Padding(
-                padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
-                child:
+                //Padding(
+                //padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
+                //child:
                 Flexible(child:
                 TextField(
                   //controller: login,
@@ -84,13 +85,14 @@ class ProfilePageState extends State<ProfilePage> {
                     InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: 'Email Address')),
-                    )),
+                    ),
 
                 ElevatedButton(
                     onPressed: () {},
-                    child: IconButton(onPressed: () {}, icon: Icon(Icons.email))
-                )
+                    child: IconButton(onPressed: () {}, icon: Icon(Icons.email)))
               ])
             ])
-        ));
-  }}
+        )
+    );
+  }
+}
